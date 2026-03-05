3cd16e7e6591ebca6374cdc757a2d48bb5e1205b
Mohammed Rayyan <129315741+mohammedrayyan-dev@users.noreply.github.com>
2026-03-06 04:13:58 +0530

    feat: functional hamburger menu navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,8 @@ const Navbar = () => {
 
     const [scrolled, setScrolled] = useState(false)
 
+    const [menuOpen, setMenuOpen] = useState(false)
+
     useEffect(() => {
         const handleScroll = () => {
             setScrolled(window.scrollY > 50)
@@ -26,7 +28,7 @@ const Navbar = () => {
   return (
 
     <header className={`sticky top-0 z-50 transition-all duration-300
-     ${scrolled 
+     ${scrolled
         ? "border-b border-white/10 bg-transparent backdrop-blur-md"
         : "border-b border-transparent bg-transparent "}`}
         >
@@ -54,14 +56,28 @@ const Navbar = () => {
             <a href="https://github.com/mohammedrayyan-dev" target="_blank" className="flex items-center justify-center w-4 md:w-6 cursor-pointer rounded-lg">
             <img src={GithubIcon} alt="Github Icon" className="w-full h-full object-contain" />
             </a>
+
             {/* Hamburger Menu */}
             <div className="block md:hidden">
-                <Menu className="text-gray-400"/>
+                <button
+                className="flex"
+                onClick={() => setMenuOpen(!menuOpen)}>
+                <Menu className="text-gray-400 w-5 h-5"/>
+                </button>
             </div>
-        </div>
 
         </div>
 
+        </div>
+
+        {menuOpen && (
+        <div className="absolute md:hidden flex flex-col items-center gap-2 top-16 right-2 bg-[#black90] border border-white/10 text-[#C9D1E3] text-sm p-3 backdrop-blur-md rounded-xl">
+            <a href="/projects">Projects</a>
+            <a href="/skills-&-tools">Skills & Tools</a>
+            <a href="/experience">Experience</a>
+            <a href="/contact">Contact</a>
+        </div>
+        )}
         
 
     </div>
