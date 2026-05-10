b79b06b3814247b2ce134f536255a850a480ec74
Mohammed Rayyan <129315741+mohammedrayyan-dev@users.noreply.github.com>
2026-05-11 01:22:15 +0530

    ui: new projects design

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,24 +10,62 @@ import ProfinaIpad from "../assets/images/devices/profina-ipad.webp"
 import ProfinaIphone from "../assets/images/devices/profina-iphone.webp"
 import TocosDesktop from "../assets/images/devices/tocos-desktop.webp"
 import { motion } from "framer-motion"
+import Container from "../components/common/Container"
+import { ArrowRight } from "lucide-react"
+
+const FinishedProjects = [
+  { 
+    id: 1,
+    title: "Qwest",
+    description: "A modern Todo App with user authentication. Users can sign up, log in, and manage their tasks efficiently.",
+    mimg: QwestIphone,
+    dimg: QwestDesktop,
+    timg: QwestIpad,
+    live: "https://www.qwest-seven.vercel.app/",
+    github: "https://github.com/mohammedrayyan-dev/Qwest.git"
+   },
+   { 
+    id: 2,
+    title: "Lulu Furniture",
+    description: "Production-ready React website for a real furniture business with responsive UI, reusable components, and performance optimizations.",
+    mimg: LuluIphone,
+    dimg: LuluDesktop,
+    timg: LuluIpad,
+    live: "https://www.lulufurniture.in/",
+    github: "https://github.com/mohammedrayyan-dev/Lulu-Furniture.git"
+   },
+   { 
+    id: 3,
+    title: "Profina",
+    description: "A digital profile management platform that allows users to view, edit, and manage their personal or professional profiles in a modern and intuitive way. It centralizes user data such as profile information, contact details, and making it easy to maintain an up-to-date digital identity.",
+    mimg: ProfinaIphone,
+    dimg: ProfinaDesktop,
+    timg: ProfinaIpad,
+    live: "https://www.profina.vercel.app/",
+    github: "https://github.com/mohammedrayyan-dev/Profina.git"
+   },
+  ]
 
 const Projects = () => {
   return (
     <>
-    <div className="relative bg-gradient-to-b from-[#141C2B] to-transparent overflow-hidden pt-10 lg:pt-20 pb-40 px-5 md:px-10 lg:px-20"> 
-    
-      <img src={Grid} alt="Grid" className="absolute inset-0 w-full object-contain [mask-image:linear-gradient(to_bottom,black,transparent)]"/>
 
-    <div className="relative flex flex-col gap-10 max-w-6xl mx-auto">
+    <Container>
+
+    <div className="overflow-hidden pt-10 lg:pt-20 pb-40"> 
+
+    <div className="flex flex-col gap-10">
 
     <motion.div 
     initial={{ opacity: 0, x: -30 }}
     animate={{ opacity: 1, x: 0 }}
     transition={{ duration: 0.5 }}
     className="flex flex-col gap-4">
+
       <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-white">
         Projects
       </h1>
+
       <p className="text-[#CBD1D880] text-sm md:text-md lg:text-base text-left">
         Here are some of the projects I have worked on.
       </p>
@@ -37,124 +75,70 @@ const Projects = () => {
     initial={{ opacity: 0, y: -30}}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5 }}
-    className="border-l border-[#CBD1D820] pl-10 lg:pl-20">
+    className=" flex flex-col gap-10">
+    
+    {FinishedProjects.map((p) => (
+    <div 
+    key={p.id}
+    className="bg-[#0a0a0a] hover:bg-[#121212] border border-[#2a2a2a] hover:border-[#3a3a3a] p-5 md:p-10 rounded-2xl">
 
     <div className="flex flex-col gap-2">
-      <h2 className="text-[#7AA2F7] text-lg lg:text-xl font-semibold">
-        Qwest
+      <h2 className="text-red-500 text-lg lg:text-2xl font-semibold">
+        {p.title}
       </h2>
-      <p className="text-[#CBD1D880] text-sm lg:text-base">
-        A modern Todo App with user authentication. Users can sign up, log in, and manage their tasks efficiently.
+      <p className="text-[#666666] text-sm lg:text-base">
+        {p.description}
       </p>
     </div>
 
     <div className="flex flex-col lg:flex-row justify-center items-center py-10 gap-10">
-      <img src={QwestDesktop} alt="Qwest Desktop" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
-      <img src={QwestIpad} alt="Qwest Ipad" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
-      <img src={QwestIphone} alt="Qwest Iphone" className="max-w-40 md:max-w-60 lg:max-w-80 max-h-40 md:max-h-60 lg:max-h-80 object-contain"/>
+      <img src={p.dimg} alt={`${p.title} Desktop`} className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
+      <img src={p.timg} alt={`${p.title} Ipad`} className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
+      <img src={p.mimg} alt={`${p.title} Iphone`} className="max-w-40 md:max-w-60 lg:max-w-80 max-h-40 md:max-h-60 lg:max-h-80 object-contain"/>
     </div>
 
     <div className="flex flex-row gap-4">
       <a
-      href="https://www.qwest-seven.vercel.app/"
+      href={p.live}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-sm lg:text-md text-[#E6EDF7] hover:text-[#7AA2F7] hover:underline underline-offset-4 transition-all duration-300">
-        View Project
+      className="text-sm lg:text-base text-[#6b6b6b] hover:text-[#ef4444] flex flex-row gap-2 items-center hover:bg-[#2a1515] border border-[#2a2a2a] hover:border-[#ef4444] p-3 rounded-xl transition-all duration-300">
+        View Live
+        <ArrowRight />
       </a>
       <a
-      href="https://github.com/mohammedrayyan-dev/Qwest.git"
+      href={p.github}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-sm lg:text-md text-[#E6EDF7] hover:text-[#7AA2F7] hover:underline underline-offset-4 transition-all duration-300">
-        View Code
+      className="text-sm lg:text-base text-[#6b6b6b] hover:text-[#ef4444] flex flex-row gap-2 items-center hover:bg-[#2a1515] border border-[#2a2a2a] hover:border-[#ef4444] p-3 rounded-xl transition-all duration-300">
+        Github
+        <ArrowRight />
       </a>
     </div> 
 
-    <div className="border-b border-[#CBD1D820] my-10"/>
-
-    <div className="flex flex-col gap-2">
-      <h2 className="text-[#7AA2F7] text-lg lg:text-xl font-semibold">
-        Lulu Furniture
-      </h2>
-      <p className="text-[#CBD1D880] text-sm lg:text-base">
-        Production-ready React website for a real furniture business with responsive UI, reusable components, and performance optimizations.
-      </p>
     </div>
+    ))}
 
-    <div className="flex flex-col lg:flex-row justify-center items-center py-10 gap-10">
-      <img src={LuluDesktop} alt="Lulu Desktop" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
-      <img src={LuluIpad} alt="Lulu Ipad" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
-      <img src={LuluIphone} alt="Lulu Iphone" className="max-w-40 md:max-w-60 lg:max-w-80 max-h-40 md:max-h-60 lg:max-h-80 object-contain"/>
-    </div>
+    <div className="border-b border-[#2a2a2a] my-8"/>
 
-    <div className="flex flex-row gap-4">
-      <a
-      href="https://www.lulufurniture.in/"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-sm lg:text-md text-[#E6EDF7] hover:text-[#7AA2F7] hover:underline underline-offset-4 transition-all duration-300">
-        View Project
-      </a>
-      <a
-      href="https://github.com/mohammedrayyan-dev/Lulu-Furniture.git"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-sm lg:text-md text-[#E6EDF7] hover:text-[#7AA2F7] hover:underline underline-offset-4 transition-all duration-300">
-        View Code
-      </a>
-    </div>
+    <div className="flex flex-col gap-5">
 
-    <div className="border-b border-[#CBD1D820] my-10"/>
+        <h2 className="text-2xl font-bold text-white">
+          Want to see the code?
+        </h2>
 
-    <div className="flex flex-col gap-2">
-      <h3 className="text-[#7AA2F7] text-lg lg:text-xl font-semibold">
-        Profina
-      </h3>
-      <p className="text-[#CBD1D880] text-sm lg:text-base">
-        A digital profile management platform that allows users to view, edit, and manage their personal or professional profiles in a modern and intuitive way. It centralizes user data such as profile information, contact details, and making it easy to maintain an up-to-date digital identity.
-      </p>
-    </div>
+        <div className="flex flex-col gap-6">
+          <p className="text-[#666666] text-sm md:text-md lg:text-base text-left">
+            All projects are on GitHub. Click any "GitHub →" button above or check my profile for the full portfolio
+          </p>
+          <button
+          onClick={() => window.open("https://github.com/mohammedrayyan-dev", "_blank")}
+          className="flex flex-row items-center justify-center gap-2 p-3 w-[200px] bg-red-500 hover:bg-red-400 text-[#ffffff] text-sm md:text-base font-bold rounded-lg transition-all duration-300">
+            View all projects
+            <ArrowRight />
+          </button>
+        </div>
 
-    <div className="flex flex-col lg:flex-row justify-center items-center py-10 gap-10">
-      <img src={ProfinaDesktop} alt="Profina Desktop" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
-      <img src={ProfinaIpad} alt="Profina Ipad" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
-      <img src={ProfinaIphone} alt="Profina Iphone" className="max-w-40 md:max-w-60 lg:max-w-80 max-h-40 md:max-h-60 lg:max-h-80 object-contain"/>
-    </div>
-
-    <div className="flex flex-row gap-4">
-      <a
-      href="https://profina.vercel.app/"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-sm lg:text-md text-[#E6EDF7] hover:text-[#7AA2F7] hover:underline underline-offset-4 transition-all duration-300">
-        View Project
-      </a>
-      <a
-      href="https://github.com/mohammedrayyan-dev/Profina.git"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-sm lg:text-md text-[#E6EDF7] hover:text-[#7AA2F7] hover:underline underline-offset-4 transition-all duration-300">
-        View Code
-      </a>
-    </div>
-
-    <div className="border-b border-[#CBD1D820] my-10"/>
-
-    <div className="flex flex-col gap-2">
-      <h2 className="text-slate-400 text-lg lg:text-xl">
-       Upcoming Project...
-      </h2>
-      <h2 className="text-slate-400 text-lg lg:text-xl font-semibold">
-       Tocos Arachnid
-      </h2>
-      <p className="text-slate-400 text-sm lg:text-base">
-        A full-stack e-commerce platform for an exotic pet store specializing in tarantulas — featuring product listings, species info, and a seamless checkout experience with login feature.
-      </p>
-    </div>
-
-    <div className="flex flex-col lg:flex-row justify-center items-center py-10 gap-10">
-      <img src={TocosDesktop} alt="Lulu Desktop" className="max-w-40 md:max-w-60 lg:max-w-80 object-contain"/>
     </div>
 
     </motion.div>
@@ -162,6 +146,9 @@ const Projects = () => {
     </div>
 
     </div>
+
+    </Container>
+
     </>
   )
 }
