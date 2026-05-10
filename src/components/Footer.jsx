e269d3576d3c925e8be8a94e408b21b3662ed2b9
Mohammed Rayyan <129315741+mohammedrayyan-dev@users.noreply.github.com>
2026-05-10 23:05:39 +0530

    ui: re-design footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,13 @@
 
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-b from-transparent to-[#141C2B] px-5 md:px-6 lg:px-20">
+    <div className="px-5 md:px-6 lg:px-20 mt-20">
 
         <div className="max-w-7xl mx-auto">
 
-        <div className="flex flex-col gap-4 pb-[20px]">
-            <h1 className="text-[#CBD1D880] text-xs md:text-sm">
-                About Me
-            </h1>
-            <p className="text-[#E6E8EB90] text-xs md:text-md">
-                I'm Rayyan, a <span className="text-[#E6E8EB]">front-end developer</span> who loves intuitive, <br/>
-                clean and modern UI design.
-            </p>
-        </div>
-
         <div className="border-t border-[#E6E8EB20] pb-4" />
 
-            <p className="text-[#E6E8EB20] text-right text-xs lg:text-sm pb-6">
+            <p className="text-[#3a3a3a] text-right text-xs lg:text-sm pb-6">
                 © 2026, Mohammed Rayyan
             </p>
 
