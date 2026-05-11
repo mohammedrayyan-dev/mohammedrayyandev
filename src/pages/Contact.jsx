403fae2900f4dbebfaef9aeb30f27ff47ce30117
Mohammed Rayyan <129315741+mohammedrayyan-dev@users.noreply.github.com>
2026-05-11 20:49:04 +0530

    ui: new contact page design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,15 @@
-
 import { motion } from "framer-motion"
+import Container from "../components/common/Container"
 
 const Contact = () => {
   return (
     <>
-    <div className="relative bg-gradient-to-b from-[#141C2B] to-transparent overflow-hidden pt-10 lg:pt-20 pb-40 md:pb-80 px-5 md:px-10 lg:px-20"> 
-    
-    
 
-    <div className="relative flex flex-col gap-10 max-w-6xl mx-auto">
+    <Container>
+
+    <div className="min-h-[calc(100vh-80px)] pt-10 lg:pt-20 pb-40 md:pb-80"> 
+    
+    <div className="flex flex-col gap-10">
 
     <motion.div 
     initial={{ opacity: 0, x: -30 }}
@@ -18,7 +19,7 @@ const Contact = () => {
       <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-white">
         Contact
       </h1>
-      <p className="text-[#CBD1D880] text-sm lg:text-base">
+      <p className="text-[#666666] text-sm lg:text-base">
         Feel free to reach out for opportunities, collaborations, or project discussions.
       </p>
     </motion.div>
@@ -27,19 +28,19 @@ const Contact = () => {
     initial={{ opacity: 0, y: -30 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5 }}
-    className="border-l border-[#CBD1D820] pl-10 lg:pl-20"> 
+    className=""> 
 
     <div
     onClick={() => window.open("mailto:mohammedrayyandmr@gmail.com", "_blank")} 
-    className="bg-[#141C2B] border border-[#2A3441] rounded-lg p-5 hover:bg-[#1B2434] cursor-pointer transition-colors duration-300">
+    className="bg-[#0a0a0a] hover:bg-[#121212] border border-[#2a2a2a] hover:border-[#3a3a3a] rounded-2xl p-5 cursor-pointer transition-colors duration-300">
       <div className="flex flex-col gap-2">
-      <h2 className="text-[#7AA2F7] text-lg lg:text-xl md:text-3xl font-semibold">
+      <h2 className="text-red-500 text-lg lg:text-xl md:text-3xl font-semibold">
         Email
       </h2>
-      <p className="text-[#CBD1D8] text-sm lg:text-base">  
+      <p className="text-[#ffffff] text-sm lg:text-base">  
         mohammedrayyandmr@gmail.com
       </p> 
-      <p className="text-[#CBD1D880] text-xs lg:text-sm">  
+      <p className="text-[#666666] text-xs lg:text-sm">  
         Click to send a message
       </p> 
       </div>
@@ -50,6 +51,9 @@ const Contact = () => {
     </div>
 
     </div>
+
+    </Container>
+
     </>
   )
 }
